9783163bf1b6ef4659409ad7936329c44e5dfbcf
blitzkz23 <blitzaldykazu@gmail.com>
2021-09-12 11:08:49 +0700

    Finished the RecyclerView project

--- RecyclerView/app/src/main/java/com/app/myrecyclerview/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/app/myrecyclerview/MainActivity.kt
@@ -4,16 +4,20 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 	private lateinit var rvHeroes: RecyclerView
 	private var list: ArrayList<Hero> = arrayListOf()
+	private var title: String = "Mode List"
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		setActionBarTitle(title)
 
 		rvHeroes = findViewById(R.id.activity_main_rv_heroes)
 		rvHeroes.setHasFixedSize(true)
@@ -22,12 +26,47 @@ class MainActivity : AppCompatActivity() {
 		showRecyclerList()
 	}
 
+	private fun showSelectedHero(hero: Hero) {
+		Toast.makeText(this, "Kamu memilih " + hero.name, Toast.LENGTH_SHORT).show()
+	}
+
+	private fun setActionBarTitle(title: String) {
+		supportActionBar?.title = title
+	}
+
 	private fun showRecyclerList() {
 		rvHeroes.layoutManager = LinearLayoutManager(this)
 		val listHeroAdapter = ListHeroAdapter(list)
 		rvHeroes.adapter = listHeroAdapter
+
+		listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback {
+			override fun onItemClicked(data: Hero) {
+				showSelectedHero(data)
+			}
+		})
 	}
 
+	private fun showRecyclerGrid() {
+		rvHeroes.layoutManager = GridLayoutManager(this, 2)
+		val gridHeroAdapter = GridHeroAdapter(list)
+		rvHeroes.adapter = gridHeroAdapter
+
+		gridHeroAdapter.setOnItemClickCallback(object : GridHeroAdapter.OnItemClickCallback {
+			override fun onItemClicked(data: Hero) {
+				showSelectedHero(data)
+			}
+		})
+	}
+
+	private fun showRecyclerCardView() {
+		rvHeroes.layoutManager = LinearLayoutManager(this)
+		val cardViewHeroAdapter = CardViewHeroAdapter(list)
+		rvHeroes.adapter = cardViewHeroAdapter
+	}
+
+	/**
+	 * This function and below are related to triple dots menu on the Action bar
+	 */
 	override fun onCreateOptionsMenu(menu: Menu?): Boolean {
 		menuInflater.inflate(R.menu.menu_main, menu)
 		return super.onCreateOptionsMenu(menu)
@@ -41,16 +80,20 @@ class MainActivity : AppCompatActivity() {
 	private fun setMode(selectedMode: Int) {
 		when (selectedMode) {
 			R.id.action_list -> {
+				title = "Mode List"
 				showRecyclerList()
 			}
 
 			R.id.action_grid -> {
-
+				title = "Mode Grid"
+				showRecyclerGrid()
 			}
 
 			R.id.action_cardview -> {
-
+				title = "Mode CardView"
+				showRecyclerCardView()
 			}
 		}
+		setActionBarTitle(title)
 	}
 }
